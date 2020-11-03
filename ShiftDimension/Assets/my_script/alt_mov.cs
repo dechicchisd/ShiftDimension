f82daa72c19958a2eaf37daebcfdf41f9ec0a906
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2020-11-03 02:31:49 +0100

    inizio shop

---
+++ ShiftDimension/Assets/my_script/alt_mov.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alt_mov : MonoBehaviour
+{
+    public Rigidbody2D pl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            pl.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+        }
+
+        else if (Input.GetKey("d"))
+        {
+            transform.Translate(0.07f, 0, 0);
+        }
+        else if (Input.GetKey("a"))
+        {
+            transform.Translate(-0.07f, 0, 0);
+        }
+    }
+}
