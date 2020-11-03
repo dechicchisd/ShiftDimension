bddbc08a766f1aea9c5a860ab36b7cdfc4d7a5d9
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2020-11-03 22:50:15 +0100

    aggiunto setting dinamico della skin e pay button in confirm purchase

---
+++ ShiftDimension/Assets/my_script/SettingImage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingImage : MonoBehaviour
+{
+    public Sprite spriteToUse;
+    public Image imgToChange;
+
+    public void ChangeImg() 
+    {
+        imgToChange.GetComponent<Image>().sprite = spriteToUse;
+    }
+
+}
