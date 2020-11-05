f5f2e2b7cd6ec7dcd12c0229d6d3a5950f475e35
delfino <dechicchisd@gmail.com>
2020-11-05 23:08:54 +0100

    aggiunto counter del gold corrente che viene decrementato ad ogni acquisto, e aggiunto un background scuro che impedisce azioni sulla finestra principale quando Ã aperto ConfirmPurcase

---
+++ ShiftDimension/Assets/my_script/UpdateMoney.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UpdateMoney : MonoBehaviour
+{
+    public TextMeshProUGUI startingMoney;
+    public TextMeshProUGUI priceText;
+    private int endMoney;
+    private int currentMoney;
+    private bool isDecreasing = false;
+    public Button bottone;
+    private IEnumerator ien;
+    // Start is called before the first frame update
+
+   
+
+    public void SettingMoney()
+    {
+        int startMoney = System.Int16.Parse(startingMoney.text);
+        int currentMoney = startMoney - System.Int16.Parse(priceText.text);
+        startingMoney.text = currentMoney.ToString();
+    }
+
+}
