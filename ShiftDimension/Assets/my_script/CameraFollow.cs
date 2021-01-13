4c823ef043f3478196e7314f0d224d866aac3bd1
gagliardi-matteo <gagliardimatteo8@gmail.com>
2021-01-14 00:35:14 +0100

    aggiunta animazioni alieni, fixato il movimento verticale della camere (ora è fisso verticalmente) e aggiunto la parallasse sulle montagne del livello 1 e livello 1 void

--- ShiftDimension/Assets/my_script/CameraFollow.cs
+++ ShiftDimension/Assets/my_script/CameraFollow.cs
@@ -6,10 +6,43 @@ public class CameraFollow : MonoBehaviour
 {
     public Vector3 offset;
     public Rigidbody2D target;
+    public Rigidbody2D zero_camera;
+    public Rigidbody2D zero_limite_superiore;
+    public Rigidbody2D zero_limite_inferiore;
+    public Rigidbody2D uno_camera;
+    public float posizioneCamera;
+
+
+     void start()
+    {
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y, offset.z);
+        posizioneCamera = target.position.y;
+    }
     // Update is called once per frame
     void LateUpdate()
     {
         if(target != null)
-            transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z);
+        {
+            if(target.position.y < zero_limite_superiore.position.y && target.position.y > zero_limite_inferiore.position.y)
+            {
+                if (transform.position.y != zero_camera.position.y)
+                {
+                    posizioneCamera = transform.position.y - 0.5f;
+                }
+                transform.position = new Vector3(target.position.x + offset.x, posizioneCamera + offset.y, offset.z);
+            }
+            else if (target.position.y <= zero_limite_inferiore.position.y)
+            {
+                
+                if (transform.position.y > uno_camera.position.y)
+                {
+                    posizioneCamera = transform.position.y - 0.5f;
+                }
+                transform.position = new Vector3(target.position.x + offset.x, posizioneCamera, offset.z);
+            }
+        }
+        
+            
+
     }
 }
--- ShiftDimension/Assets/my_script/Enemy.cs
+++ ShiftDimension/Assets/my_script/Enemy.cs
@@ -11,28 +11,45 @@ public class Enemy : MonoBehaviour
     public Rigidbody2D rightBound; 
     public float walkingSpeed;
     private bool isRight = false;
+    private bool isTurnedR = false;
+    private bool isTurnedL = true;
+    public Animator animazione;
 
     // Update is called once per frame
     protected virtual void Update()
     {
         if(isRight == false)
         {
+            if(isTurnedL == false)
+            {
+                transform.Rotate(0, 180f, 0);
+                isTurnedL = true;
+            }
+            animazione.Play("alienWalk");
             alien.transform.Translate(-walkingSpeed * Time.deltaTime, 0, 0, Space.World);
         }
 
         else if (isRight == true)
         {
+            if (isTurnedR == false)
+            {
+                transform.Rotate(0, 180f, 0);
+                isTurnedR = true;
+            }
+            animazione.Play("alienWalk");
             alien.transform.Translate(walkingSpeed * Time.deltaTime, 0, 0, Space.World);
         }
 
         if (alien.position.x < leftBound.position.x)
         {
             isRight = true;
+            isTurnedL = false;
         }
 
         if (alien.position.x > rightBound.position.x)
         {
             isRight = false;
+            isTurnedR = false;
         }
     }
 
