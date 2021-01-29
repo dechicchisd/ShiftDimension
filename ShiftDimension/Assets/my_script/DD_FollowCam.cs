7e0353610d072f2520317db02cdae801fdb5af96
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2021-01-29 19:00:57 +0100

    aggiunte finestre per login

---
+++ ShiftDimension/Assets/my_script/DD_FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_FollowCam : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.position = new Vector3(player.position.x, player.position.y) + offset;
+        }
+    }
+}
