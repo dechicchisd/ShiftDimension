c42f97cd9611dd153bc45b802659dda5dd16d199
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2021-01-30 06:25:44 +0100

    aggiunto script per interagire col db (ben lontano dal funzionare)

--- ShiftDimension/Assets/my_script/AccountManagement.cs
+++ ShiftDimension/Assets/my_script/AccountManagement.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking; 
 
 public class AccountManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button RegisterButton;
+    public InputField emailField;
+    public InputField pswField;
+
+    public void CallRegister()
     {
-        
+        StartCoroutine(Register());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator Register()
     {
-        
+        WWWForm form = new WWWForm();
+        form.AddField("email", emailField.text);
+        form.AddField("pass", pswField.text);
+        UnityWebRequest www = new UnityWebRequest("https://mysql.aruba.it/login/index.php?lang=it-iso-8859-1");
+        yield return www;
+
+
     }
 }
