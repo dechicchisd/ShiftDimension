e81b095fc8881e791fde049d6894d33662bb2e3e
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2021-05-11 14:04:54 +0200

    i pulsanti aprono e chiudono i menu in tutti i livelli, e risolti i null reference exception

--- ShiftDimension/Assets/my_script/MobileMov.cs
+++ ShiftDimension/Assets/my_script/MobileMov.cs
@@ -39,9 +39,9 @@ public class MobileMov : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        textCoin.text = PlayerPrefs.GetFloat("coins").ToString("0");
-     
-        if(isShifting == false)
+        textCoin.text = PlayerPrefs.GetFloat("coins").ToString("0");    
+
+        if (isShifting == false)
         {
             transform.position = inizioLivello.position + new Vector2(0, 2f);
         }
